95e41d3e4f71f07110aac1270a2c603c9efaf215
Farhad Siraj <farhadsiraj@gmail.com>
2024-08-27 14:32:17 -0400

    Added dynamic rating color

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -5,6 +5,12 @@ export default function MovieCard({ movie }) {
   const handleError = (e) => {
     e.target.src = "images/default.jpg";
   };
+
+  const getRatingClass = (rating) => {
+    if (rating >= 8) return "rating-good";
+    if (rating >= 5 && rating < 8) return "rating-ok";
+    return "rating-bad";
+  };
   return (
     <div key={movie.id} className="movie-card">
       <img
@@ -15,7 +21,9 @@ export default function MovieCard({ movie }) {
       <div className="movie-card-info">
         <div className="movie-card-title">{movie.title}</div>
         <div className="movie-card-genre">{movie.genre}</div>
-        <div className="movie-card-rating">{movie.rating}</div>
+        <div className={`movie-card-rating ${getRatingClass(movie.rating)}`}>
+          {movie.rating}
+        </div>
       </div>
     </div>
   );
